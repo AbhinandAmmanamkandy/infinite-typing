0a543bc5222706fa1bf01895be4d0ca46710b0aa
AbhinandAmmanamkandy <abhinandammanamkandypvt@gmail.com>
2026-04-03 05:06:49 +0530

    Get sentence from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import {useEffect, useState} from "react";
+
 const App = () => {
+    const [sentence, setSentence] = useState("");
+
+    useEffect(() => {
+        const callAPI = async () => {
+            try {
+                const res = await fetch("https://api.quotable.io/random");
+                const data = await res.json();
+                const content = data.content;
+                setSentence(content);
+            } catch (err) {
+                console.error(err);
+            }
+        }
+        callAPI()
+    }, [])
     return (
         <div>
-            This is working
+            { sentence }
         </div>
     );
 };
