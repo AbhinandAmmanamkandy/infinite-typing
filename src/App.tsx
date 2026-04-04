a58c0dca22ed47eee3be995150eb8aa5dfcb5c5a
AbhinandAmmanamkandy <abhinandammanamkandypvt@gmail.com>
2026-04-05 01:30:59 +0530

    Only type if the characters match * Clear typed if finished and show another sentence

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,44 @@
-import {useEffect, useState} from "react";
+import {type ChangeEvent, useEffect, useState} from "react";
 
 const App = () => {
+    const [typed, setTyped] = useState("");
     const [sentence, setSentence] = useState("");
     const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        const callAPI = async () => {
-            try {
-                const res = await fetch("https://api.quotable.io/random");
-                const data = await res.json();
-                const content = data.content;
-                setSentence(content);
-            } catch (err) {
-                console.error(err);
-            } finally {
-                setLoading(false);
-            }
+    const callAPI = async (): Promise<void> => {
+        try {
+            const res = await fetch("https://api.quotable.io/random");
+            const data = await res.json();
+            const content = data.content;
+            setSentence(content);
+        } catch (err) {
+            console.error(err);
+        } finally {
+            setLoading(false);
         }
+    }
+
+    useEffect(() => {
         callAPI()
     }, [])
+    const checkTyped = (e: ChangeEvent<HTMLInputElement>) => {
+        if (sentence === e.currentTarget.value) {
+            callAPI();
+            setTyped("");
+            return;
+        }
+        if (sentence[e.currentTarget.value.length - 1] === e.currentTarget.value[e.currentTarget.value.length - 1]) {
+            setTyped(e.target.value)
+        }
+    }
     return (
         <div>
-            { loading ? "Loading..." : sentence }
+            {loading ? "Loading..." : sentence}
+            <br/>
+            <input
+                type="text"
+                value={typed}
+                onChange={checkTyped}/>
         </div>
     );
 };
