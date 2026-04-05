c7b0444e6e559aa2a2cfaa9cd21940d50e9a72e5
AbhinandAmmanamkandy <abhinandammanamkandypvt@gmail.com>
2026-04-05 09:06:36 +0530

    Clean up to update algorithm to not to include input type text

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
 import {useSentence} from "./hooks/useSentence.ts";
-import {checkTyping} from "./utils/checkTyping.ts";
-import {type ChangeEvent, useEffect, useState} from "react";
+import {useEffect} from "react";
 
 const App = () => {
-    const [typed, setTyped] = useState("");
     const {sentence, loading, fetchSentence} = useSentence();
 
     useEffect(() => {
-        fetchSentence();
-    }, []);
+        fetchSentence().then(() => {
+        });
+    }, [fetchSentence]);
 
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        const value = e.target.value;
-
-        if (checkTyping(value, sentence)) {
-            setTyped(value);
-        }
-
-        if (value === sentence) {
-            setTyped("");
-            fetchSentence();
-        }
-    };
     return (
-        <div>
+        <div className="flex items-center justify-center h-screen flex-col">
             {loading ? "Loading..." : sentence}
-            <br/>
-            <input
-                type="text"
-                value={typed}
-                onChange={handleChange}/>
         </div>
     );
 };
