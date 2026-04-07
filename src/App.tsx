4b3efd4a5c1f668fe0f0d042de7dc7d7b210d58b
AbhinandAmmanamkandy <abhinandammanamkandypvt@gmail.com>
2026-04-07 10:59:13 +0530

    Check sentence equality using a text box

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import {useEffect, useRef, useState} from "react";
 
 const getSentence = () => {
     const words = [
@@ -17,55 +17,47 @@ const getSentence = () => {
 };
 
 export default function App() {
-    const [sentence, setSentence] = useState("");
     const [typed, setTyped] = useState("");
-    const [loading, setLoading] = useState(true);
-
-    const nextRef = useRef("");
-    const divRef = useRef<HTMLDivElement>(null);
+    const [sentence, setSentence] = useState("");
+    const inputRef = useRef<HTMLInputElement | null>(null);
 
     useEffect(() => {
         const init = () => {
-            const first = getSentence();
-            nextRef.current = getSentence();
-
-            setSentence(first);
-            setLoading(false);
-            divRef.current?.focus();
+            setSentence(getSentence());
+            inputRef.current?.focus();
         };
 
         init();
     }, []);
 
-    const handleKeyDown = async (e: React.KeyboardEvent) => {
-        const value = typed + e.key;
-
-        if (!sentence.startsWith(value)) return;
-
-        if (value === sentence) {
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setTyped(e.currentTarget.value);
+        if (e.currentTarget.value === sentence) {
+            setSentence(getSentence());
             setTyped("");
-            setSentence(nextRef.current);
-            nextRef.current = await getSentence();
-        } else {
-            setTyped(value);
         }
-    };
+    }
 
     return (
-        <div
-            ref={divRef}
-            tabIndex={0}
-            onKeyDown={handleKeyDown}
-            className="flex items-center justify-center h-screen text-4xl flex-col"
-        >
-            {!loading && (
-                <p>
-                    <span className="text-green-500">
-                        {sentence.slice(0, typed.length)}
-                    </span>
-                    <span>{sentence.slice(typed.length)}</span>
-                </p>
-            )}
+        <div className="flex flex-col items-center justify-center h-screen text-4xl whitespace-nowrap">
+            <div className="">
+                {sentence.split("").map((char, index) => {
+                    let color = "";
+                    if (index < typed.length) {
+                        if (typed[index] === char) {
+                            color += "text-green-500";
+                        } else {
+                            color += "text-red-500";
+                        }
+                    }
+                    return (
+                        <span key={index} className={color}>
+                            {char}
+                        </span>
+                    );
+                })}
+            </div>
+            <input ref={inputRef} type="text" onChange={handleChange} value={typed}/>
         </div>
     );
 }
